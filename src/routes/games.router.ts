c8f7a871e0fe20e00f239ec27d8a3f34ba2458d5
Josh <jamesjoshualazo@gmail.com>
2025-02-14 23:52:13 +0800

    feat(games.router.ts): wIP

--- src/routes/games.router.ts
+++ src/routes/games.router.ts
@@ -1,6 +1,6 @@
 import express, { Request, Response } from "express";
 import { ObjectId } from "mongodb";
-import { collections } from "../database.service";
+import { collections } from "../services/database.service";
 import Game from "../models/game";
 
 export const gamesRouter = express.Router();
@@ -23,12 +23,26 @@ gamesRouter.get("/:id", async (req: Request, res: Response) => {
     try {
 
         const query = { _id: new ObjectId(id) };
-        const game = (await collections.games.findOne(query)) as Game;
+        const game = (await collections.games?.findOne(query));
 
         if (game) {
             res.status(200).send(game);
         }
     } catch (error) {
         res.status(404).send(`Unable to find matching document with id: ${req.params.id}`);
+    }
+});
+
+gamesRouter.post("/", async (req: Request, res: Response) => {
+    try {
+        const newGame = req.body as Game;
+        const result = await collections.games?.insertOne(newGame);
+
+        result
+            ? res.status(201).send(`Successfully created a new game with id ${result.insertedId}`)
+            : res.status(500).send("Failed to create a new game.");
+    } catch (error: any) {
+        console.error(error);
+        res.status(400).send(error.message);
     }
 });
