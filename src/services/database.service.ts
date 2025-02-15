9126947ddbcc1c921b5f87ca7bd6980835509a3b
Josh <jamesjoshualazo@gmail.com>
2025-02-16 02:15:21 +0800

    feat(database.service.ts): wIP

--- src/services/database.service.ts
+++ src/services/database.service.ts
@@ -1,21 +1,23 @@
 import * as mongoDB from "mongodb";
 
-import dotenv from "dotenv";
+import * as dotenv from "dotenv";
 
 export const collections: { games?: mongoDB.Collection } = {};
 
 export async function connectToDatabase() {
     dotenv.config();
 
-    const client: mongoDB.MongoClient = new mongoDB.MongoClient(process.env.DB_CONN_STRING as string);
+    const dbConn: string = process.env.DB_CONN_STRING as string;
+
+    const client: mongoDB.MongoClient = new mongoDB.MongoClient(dbConn);
 
     await client.connect();
 
     const db: mongoDB.Db = client.db(process.env.DB_NAME);
 
-    const gamesCollection: mongoDB.Collection = db.collection(process.env.GAMES_COLLECTION_NAME as string);
+    const gamesCollection: mongoDB.Collection = db.collection(dbConn);
 
     collections.games = gamesCollection;
 
-    console.log(`success`);
+    console.log(`Successfully connected to database: ${db.databaseName} and collection: ${gamesCollection.collectionName}`);
 }
