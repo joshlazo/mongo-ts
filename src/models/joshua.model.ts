d2a2a47299089d4c1593958fcc31115fc984e66d
Josh <jamesjoshualazo@gmail.com>
2025-02-17 19:19:05 +0800

    feat(joshua.model.ts): add additional properties
    interesting

--- src/models/joshua.model.ts
+++ src/models/joshua.model.ts
@@ -1,4 +1,8 @@
 export interface Joshua {
     "bomb" : string;
     "boing" : string;
+<<<<<<< HEAD
+=======
+    "badog" : string;
+>>>>>>> ce77d6b (feat(joshua.model.ts): add additional properties)
 }
