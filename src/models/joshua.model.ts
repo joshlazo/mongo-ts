1b0599f40d2e6224915ce60862688de2bedf3765
Josh <jamesjoshualazo@gmail.com>
2025-02-18 19:22:50 +0800

    test

--- src/models/joshua.model.ts
+++ src/models/joshua.model.ts
@@ -1,8 +1,5 @@
 export interface Joshua {
     "bomb" : string;
     "boing" : string;
-<<<<<<< HEAD
-=======
     "badog" : string;
->>>>>>> ce77d6b (feat(joshua.model.ts): add additional properties)
 }
